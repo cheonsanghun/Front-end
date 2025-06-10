810ba5b2ff85ea2e222ff50d118cf4126760e6e9
An-Seongmin666 <bnb34bnb77@gmail.com>
2025-06-10 16:46:08 +0900

    update name3

--- jq/script3.js
+++ jq/script3.js
@@ -1,11 +1,20 @@
 $(document).ready(function () {
-    // 총 주문 금액
-     let sum = 0;
+    let sum = 0;
+    let num = 0;
+    let kaikei = '<div id="kaikeichart"><table><tr><th class="num">No.</th><th class="name">商品</th><th class="price">値段</th><th class="quantity">数量</th></tr>'
+    // 마지막 계산
     $('#kaikei').on('click', function() {
-        alert('총 금액은 ' + sum + '엔 입니다.');
+        //alert('총 금액은 ' + sum + '엔 입니다.');
+        const confirmKaikei = confirm('お会計しますか？よろしければ「確認」ボタンを押してください。');
+        if (confirmKaikei) {
+        kaikei += `<tr><th>総計</th><th class="sum" colspan = 3>¥${sum}</th></tr></table></div>`
+        $('#kaikeiscreen').html(kaikei);
+        $('#top3').html('ありがとうございました。');
+      }
     });
+    // 직원 호출
     $('#yobidasi').on('click', function() {
-        alert('지금 직원을 호출 중입니다. 잠시만 기다려 주세요');
+        alert('只今呼び出し中です。少々お待ちください。');
     });
     // 초기화면
     categoryChange('food');
@@ -25,14 +34,18 @@ $(document).ready(function () {
         
         if (!product) return; // 제품을 찾을 수 없으면 종료
 
-        const confirmOrder = confirm('주문하시겠습니까?');
+        const confirmOrder = confirm(`「${product.name}」を「${$(`#quan-${product.id}`).val()}個」注文でよろしいでしょうか？`);
         if (confirmOrder) {
+            let name = product.name;
             let quantity = parseInt($(`#quan-${product.id}`).val());
             let price = product.price;
             sum += quantity * price;
-            console.log('주문 금액:', sum);
-            console.log('수량:', quantity);
-            console.log('가격:', price);
+            num++;
+            kaikei += `<tr><td class="num">${num}</td><td> ${name}</td><td class="price">¥${price}</td><td class="quantity">${quantity}</td></tr>`
+            //console.log('주문 금액:', sum);
+            //console.log('수량:', quantity);
+            //console.log('가격:', price);
+            alert('注文を転送しました。');
         }
     });
     
@@ -53,7 +66,7 @@ $(document).ready(function () {
         if (remainingTime <= 0) {
           clearInterval(countdownInterval);
           $('#remaining-time').text('');
-          alert('L.O시간이 종료되었습니다.');
+          alert('L.O時間が終了しました。');
         } else {
           remainingTime--;
         }
@@ -63,9 +76,13 @@ $(document).ready(function () {
   // 메뉴 카테고리별 표시
   const products = [
     { id: 1, name: "焼き鳥5種盛り", price: 1000, category: "food", image: "" },
-    { id: 2, name: "フライドポテト", price: 600, category: "food", image: "" },
-    { id: 3, name: "コーラ", price: 350, category: "drink", image: "" },
-    { id: 4, name: "生ビール", price: 500, category: "drink", image: "" }
+    { id: 2, name: "炒飯", price: 600, category: "food", image: "" },
+    { id: 3, name: "シーザーサラダ", price: 500, category: "food", image: "" },
+    { id: 4, name: "フライドポテト", price: 350, category: "food", image: "" },
+    { id: 5, name: "コーラ", price: 300, category: "drink", image: "" },
+    { id: 6, name: "烏龍茶", price: 300, category: "drink", image: "" },
+    { id: 7, name: "生ビール", price: 500, category: "drink", image: "" },
+    { id: 8, name: "ハイボール", price: 400, category: "drink", image: "" }
   ];
 
   function categoryChange(category) {
@@ -78,12 +95,12 @@ $(document).ready(function () {
           <img src="${products.image}" alt="${products.name}">
           <div class="product-info">
             <h4>${products.name}</h4>
-            <p>¥${products.price}</p>
           </div>
-          수량 &nbsp;
+          <b>¥${products.price}</b> &nbsp;
+          数量: &nbsp;
           <input type="number" id="quan-${products.id}" class="quan" value="0" min="0" max="10">
           &nbsp;&nbsp;
-          <button id="add-${products.id}" data-price="${products.price}" class="add-btn">주문</button>
+          <button id="add-${products.id}" data-price="${products.price}" class="add-btn">注文</button>
         </div>
       `);
       $list.append($item);
